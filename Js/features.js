1275ef9e0058bbdb6b4aa688a925b1282b923f00
Mohammad Fakrul Islam <mfislam357@gmail.com>
2024-10-06 11:02:13 +0600

    setting up 'Toggle button' section

---
+++ Js/features.js
@@ -0,0 +1,6 @@
+function changeButtonColor(id){
+    document.getElementById('btn-donation').classList.remove('bg-gradient-to-tr', 'from-lime', 'via-light-lime', 'to-lime');
+    document.getElementById('btn-history').classList.remove('bg-gradient-to-tr', 'from-lime', 'via-light-lime', 'to-lime');
+
+    document.getElementById(id).classList.add('bg-gradient-to-tr', 'from-lime', 'via-light-lime', 'to-lime');
+}
--- Js/home.js
+++ Js/home.js
@@ -1,3 +1,11 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
     window.location.href = 'blog.html'
 })
+
+document.getElementById('btn-donation').addEventListener('click', function(){
+    changeButtonColor('btn-donation')
+})
+
+document.getElementById('btn-history').addEventListener('click', function(){
+    changeButtonColor('btn-history')
+})
